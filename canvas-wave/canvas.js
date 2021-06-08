f000ac896e57fb4f02a842af2efc328f60323771
Jun Joy <chojuneseo1@gmail.com>
2021-06-08 23:47:35 +0900

    update; remove comments canvas-wave

--- canvas-wave/canvas.js
+++ canvas-wave/canvas.js
@@ -1,5 +1,3 @@
-// import * as dat from 'dat.gui'
-// const gui = new dat.GUI();
 const canvas = document.querySelector("canvas");
 const c = canvas.getContext("2d");
 
@@ -26,26 +24,6 @@ const backgroundColor = {
   a: 0.01,
 };
 
-// const waveFolder = gui.addFolder("wave");
-// waveFolder.add(wave, "y", 0, canvas.height);
-// waveFolder.add(wave, "length", -0.01, 0.01);
-// waveFolder.add(wave, "amplitude", -300, 300);
-// waveFolder.add(wave, "frequency", -0.01, 1);
-// waveFolder.open();
-
-// const strokeFolder = gui.addFolder("stroke");
-// strokeFolder.add(strokeColor, "h", 0, 255);
-// strokeFolder.add(strokeColor, "s", 0, 100);
-// strokeFolder.add(strokeColor, "l", 0, 100);
-// strokeFolder.open();
-
-// const backgroundFolder = gui.addFolder("background");
-// backgroundFolder.add(backgroundColor, "r", 0, 255);
-// backgroundFolder.add(backgroundColor, "g", 0, 255);
-// backgroundFolder.add(backgroundColor, "b", 0, 255);
-// backgroundFolder.add(backgroundColor, "a", 0, 1);
-// backgroundFolder.open();
-
 let increment = wave.frequency;
 function animate() {
   requestAnimationFrame(animate);
@@ -53,7 +31,7 @@ function animate() {
   c.fillRect(0, 0, canvas.width, canvas.height);
 
   c.beginPath();
-//   c.moveTo(0, canvas.height / 2);
+  c.moveTo(0, canvas.height / 2);
 
   for (let i = 0; i < canvas.width; i++) {
     c.lineTo(
